060223e180ec56a3b40cfa79ef653752756f08f5
Your Name <claudiakosylak@gmail.com>
2023-10-09 15:04:05 -0400

    endpoints work but points not yet calculating correctly

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addReceipt } from "./receipts.js";
+import { addReceipt, getPoints } from "./receipts.js";
 
 const app = express();
 
@@ -10,6 +10,14 @@ app.post("/receipts/process", async (req, res) => {
     const receipt = await addReceipt(retailer, purchaseDate, purchaseTime, total, items);
     return res.json({
         id: receipt
+    })
+})
+
+app.get("/receipts/:id/points", async (req, res) => {
+    const { id } = req.params;
+    const points = await getPoints(id);
+    return res.json({
+        points: points
     })
 })
 
--- receipts.js
+++ receipts.js
@@ -17,3 +17,63 @@ export const addReceipt = async (retailer, purchaseDate, purchaseTime, total, it
     }
     return id;
 }
+
+const alphanumericPoints = str => {
+    let count = 0;
+    for (let char of str) {
+        if (/^[a-zA-Z0-9]+$/.test(char)) count++;
+    }
+    return count;
+}
+
+const itemCountPoints = num => {
+    if (num === 1) return 0;
+    if (num % 2 === 0) return (num / 2) * 5;
+    else return ((num - 1) / 2) * 5;
+}
+
+const itemDescriptionsPoints = items => {
+    let count = 0;
+    for (let item of items) {
+        const length = item.shortDescription.length;
+        if (length % 3 === 0) {
+            count += Math.ceil(parseFloat(item.price) * 0.2);
+        }
+    }
+    return count;
+}
+
+const purchaseDateOdd = date => {
+    const arr = date.split("-");
+    const day = parseInt(arr[2]);
+    if (day % 2 !== 0) return 6;
+    else return 0;
+}
+
+const purchaseTimePoints = time => {
+    const arr = time.split(":");
+    const hour = parseInt(arr[0]);
+    const minute = parseInt(arr[1]);
+    if ((hour >= 14 && minute > 0) && hour < 16) return 10;
+    else return 0;
+}
+
+export const getPoints = async (id) => {
+    const receipt = receipts[id];
+    let points = 0;
+    // Points for alphanumeric chars in retailer name
+    points += alphanumericPoints(receipt.retailer);
+    // Points for round dollar total
+    if (receipt.total % 1 === 0) points += 50;
+    // Points for total multiple of .25
+    if ((receipt.total / 0.25) % 1 === 0) points += 25;
+    // points for every two items on receipt
+    points += itemCountPoints(receipt.items.length);
+    // points for item descriptions
+    points += itemDescriptionsPoints(receipt.items);
+    // points for purchase date being odd
+    points += purchaseDateOdd(receipt.purchaseDate);
+    // points for purchase time
+    points += purchaseTimePoints(receipt.purchaseTime);
+    return points;
+}
